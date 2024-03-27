007db884c0c333e3346ec38bdc3f5ba5ce3d68d8
adelhpour <ad.heydarabadi@gmail.com>
2024-03-27 13:25:45 -0700

    - a comprehensive resource directory is replaced all the other directories.
    - the answers are now in the verbose mode
    
    - the question is now embellished with some words before and after it.

--- src/paperinquirer/paperinquirer.py
+++ src/paperinquirer/paperinquirer.py
@@ -5,52 +5,78 @@ from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 
 class PaperInquirer:
-    def __init__(self, pdf_dir, description_dir="", storage_dir=""):
+    def __init__(self, resource_dirs, storage_dir=""):
+        self._agent = None
         self.query_engine_tools = None
-        self.load(pdf_dir, description_dir, storage_dir)
+        self.file_list = []
+        self.load(resource_dirs, storage_dir)
 
-    def load(self, pdf_dir, description_dir="", storage_dir=""):
-        pdf_dir = self._get_valid_dir(pdf_dir)
-        if pdf_dir:
-            query_indices_info = self._create_query_indices_info(pdf_dir, description_dir, storage_dir)
-            query_engines_info = self._create_query_engines_info(query_indices_info)
-            self.query_engine_tools = self._create_query_engine_tools(query_engines_info)
+    def load(self, resource_dirs, storage_dir=""):
+        if not isinstance(resource_dirs, list):
+            resource_dirs = [resource_dirs]
+        query_indices_info = self._create_query_indices_info(resource_dirs, storage_dir)
+        query_engines_info = self._create_query_engines_info(query_indices_info)
+        self.query_engine_tools = self._create_query_engine_tools(query_engines_info)
 
     def inquire(self, question):
         response = ""
         if self.query_engine_tools is not None:
             agent = self._get_agent(self.query_engine_tools)
-            response = agent.chat(question)
+            response = agent.chat(self._embellish_question(question))
         else:
             raise ValueError("No query engine tools loaded")
         return str(response)
 
-    @staticmethod
-    def _get_agent(query_engine_tools):
-        llm = OpenAI(model="gpt-3.5-turbo-0613")
-        agent = ReActAgent.from_tools(
-            query_engine_tools,
-            llm=llm
-        )
-        return agent
+    def _get_agent(self, query_engine_tools):
+        if self._agent is None:
+            llm = OpenAI(model="gpt-3.5-turbo-0613")
+            self._agent = ReActAgent.from_tools(
+                query_engine_tools,
+                llm=llm,
+                verbose=True
+            )
 
-    def _create_query_indices_info(self, pdf_dir, description_dir, storage_dir):
-        indices_list =[]
-        for file in os.listdir(pdf_dir):
-            if file.endswith(".pdf"):
-                doc_index = self._load_index(storage_dir + file.split(".pdf")[0])
-                if doc_index is None:
-                    doc = SimpleDirectoryReader(
-                        input_files=[pdf_dir + file]
-                    ).load_data()
-                    doc_index = VectorStoreIndex.from_documents(doc)
-                    if storage_dir:
-                        storage_dir = self._get_valid_dir(storage_dir)
-                        doc_index.storage_context.persist(persist_dir=storage_dir + file.split(".pdf")[0])
-                doc_index_info = {'name': self._remove_hyphen(file.split(".pdf")[0]), 'index': doc_index,
-                                  'description': self._get_description_info(description_dir, file.split(".pdf")[0])}
-                indices_list.append(doc_index_info)
+        return self._agent
+
+    def _create_query_indices_info(self, resource_dirs, storage_dir):
+        indices_list = []
+        for resource_dir in resource_dirs:
+            resource_dir = self._get_valid_dir(resource_dir)
+            for file in os.listdir(resource_dir):
+                suffix = file.split(".")[-1]
+                if self.is_valid_suffix(suffix):
+                    self.file_list.append(file)
+                    doc_index = self._load_index(storage_dir + file.split("." + suffix)[0])
+                    if doc_index is None:
+                        doc = SimpleDirectoryReader(
+                            input_files=[resource_dir + file]
+                        ).load_data()
+                        doc_index = VectorStoreIndex.from_documents(doc)
+                        if storage_dir:
+                            storage_dir = self._get_valid_dir(storage_dir)
+                            doc_index.storage_context.persist(persist_dir=storage_dir + file.split("." + suffix)[0])
+                    doc_index_info = {'name': self.remove_space(self._remove_hyphen(file.split("." + suffix)[0])),
+                                      'index': doc_index}
+                    indices_list.append(doc_index_info)
+
         return indices_list
+
+    def _embellish_question(self, question):
+        embellished_question = ""
+        if len(self.file_list):
+            embellished_question += "Use "
+            for file_index in range(len(self.file_list)):
+                embellished_question += self.file_list[file_index].split(".")[0]
+                if file_index < len(self.file_list) - 1:
+                    if file_index == len(self.file_list) - 2:
+                        embellished_question += " and "
+                    else:
+                        embellished_question += ", "
+            embellished_question += " tools to answer the following question: " + question
+        else:
+            embellished_question = question
+
+        return embellished_question + " Think step by step."
 
     @staticmethod
     def _load_index(file_name):
@@ -67,7 +93,7 @@ class PaperInquirer:
         engines_info_list = []
         for index_info in indices_info:
             engine = index_info['index'].as_query_engine(similarity_top_k=3)
-            engine_info = {'name': index_info['name'], 'description': index_info['description'], 'engine': engine}
+            engine_info = {'name': index_info['name'], 'engine': engine}
             engines_info_list.append(engine_info)
         return engines_info_list
 
@@ -80,23 +106,11 @@ class PaperInquirer:
                     query_engine=query_engine_info['engine'],
                     metadata=ToolMetadata(
                     name=query_engine_info['name'],
-                    description=query_engine_info['description']
+                    description=query_engine_info['name']
                     ),
                 )
             )
         return tools_info_list
-
-    def _get_description_info(self, description_dir, file):
-        description_info = ""
-        if description_dir:
-            description_dir = self._get_valid_dir(description_dir)
-            abstract_file = description_dir + file + ".txt"
-            try:
-                with open(abstract_file, 'r') as file_name:
-                    return file_name.read()
-            except:
-                description_info = ""
-        return description_info
 
     @staticmethod
     def _get_valid_dir(directory):
@@ -108,5 +122,13 @@ class PaperInquirer:
         return directory
 
     @staticmethod
+    def is_valid_suffix(suffix):
+        return suffix in ["pdf", "txt", "sbml"]
+
+    @staticmethod
     def _remove_hyphen(file_name):
         return file_name.replace("-", "_")
+
+    @staticmethod
+    def remove_space(file_name):
+        return file_name.replace(" ", "_")
